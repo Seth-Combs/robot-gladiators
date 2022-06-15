94b3556803d5f174530fbc7baff0fad112e9f9b9
Seth-Combs <Scombs720@gmail.com>
2022-06-15 00:54:08 -0400

    added first attack on enemy subtracts from health

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,29 @@
-// This creates a function called "fight"
-
-function fight() {
-    window.alert(playerName + " has begun the fight! ");
-}
-
 var playerName = window.prompt("What is your Robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+console.log( playerName, playerAttack, playerHealth, playerMoney);
+
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+var enemyAttack = 8;
+
+var fight = function() {
+    window.alert("The fight has begun! ");
+    
+    enemyHealth = enemyHealth - playerAttack
+    
+    console.log (
+        playerName + " just attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+};
+
+
+
+
+
 
 
 
