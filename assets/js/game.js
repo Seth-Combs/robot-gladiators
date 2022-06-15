a95a2b8e2d91a4abf34ce7bfa13fedc3dddabf4e
Seth-Combs <Scombs720@gmail.com>
2022-06-14 23:20:16 -0400

    testing again

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// This creates a function called "fight"
+
+function fight() {
+    window.alert(playerName + " has begun the fight! ");
+}
+
+var playerName = window.prompt("What is your Robot's name?");
+
+
+
+
+fight();
